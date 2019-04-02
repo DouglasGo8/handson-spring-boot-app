be71a6b110f343851dfe8227c97eb4397c1de990
DouglasGo8 <abraham.dias.douglas@gmail.com>
2019-04-02 01:51:50 -0300

    First Commit #1

---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+# Docker
+
+docker run -d --name postgres -p 5432:5432 -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres postgres
---
+++ src/main/java/com/packtpub/handson/spring/boot/app/HandsonSpringBootAppApplication.java
@@ -0,0 +1,23 @@
+package com.packtpub.handson.spring.boot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * 
+ * @author DouglasdB
+ *
+ */
+@SpringBootApplication
+public class HandsonSpringBootAppApplication {
+
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(HandsonSpringBootAppApplication.class, args);
+	}
+
+}
